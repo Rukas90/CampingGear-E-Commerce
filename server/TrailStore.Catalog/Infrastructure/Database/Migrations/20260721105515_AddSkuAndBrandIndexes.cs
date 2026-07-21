279f802fc53d320f3e9908862e66a0825e49b82c
Rukas Skirkevicius <rukas.skirkevicius@gmail.com>
2026-07-21 13:55:58 +0300

    Add catalog index migration

---
+++ server/TrailStore.Catalog/Infrastructure/Database/Migrations/20260721105515_AddSkuAndBrandIndexes.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TrailStore.Catalog.Infrastructure.Database.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddSkuAndBrandIndexes : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateIndex(
+                name: "IX_Skus_Stock",
+                schema: "catalog",
+                table: "Skus",
+                column: "Stock");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Skus_UnitPrice",
+                schema: "catalog",
+                table: "Skus",
+                column: "UnitPrice");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Brands_Slug",
+                schema: "catalog",
+                table: "Brands",
+                column: "Slug",
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_Skus_Stock",
+                schema: "catalog",
+                table: "Skus");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Skus_UnitPrice",
+                schema: "catalog",
+                table: "Skus");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Brands_Slug",
+                schema: "catalog",
+                table: "Brands");
+        }
+    }
+}
